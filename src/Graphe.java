ff29849f3472c3c00794ce5cf76d96794fafc7c6
Tennessy Kolubako <ten.perso@gmail.com>
2013-01-05 17:31:48 +0100

    Restructuration du code + ajout interface

--- src/Graphe.java
+++ src/Graphe.java
@@ -1,4 +1,15 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.LinkedList;
+import java.util.Map;
 
 public class Graphe {
 	protected ArrayList<Sommet> listeSommet;
@@ -43,12 +54,14 @@ public class Graphe {
 		return nbUser;
 	}
 
-	public void addUser(int age, String name){
-		listeSommet.add(new User(this.lastId, age, name));
+	public User addUser(int age, String name){
+		User u = new User(this.lastId, age, name);
+		listeSommet.add(u);
 		lastId++;
+		return u;
 	}
 
-	public boolean addUser(int id, int age, String name){
+	public void addUser(int id, int age, String name){
 		boolean exist = false;
 		for(Sommet s : this.listeSommet){
 			if(s.getNumero() == id){
@@ -59,18 +72,14 @@ public class Graphe {
 			listeSommet.add(new User(id, age, name));
 			if(id>this.lastId)
 				this.lastId = id;
-			return true;
 		}
-		return false;
 	}
 	
-	public void addPage(String name, User admin){
-		if(!listeSommet.contains(new Page(0,name, admin))){
-			listeSommet.add(new Page(this.lastId, name, admin));
-			lastId++;
-		}
-		else
-			System.out.println("La page '"+name+"' existe déja");
+	public Page addPage(String name, User admin){
+		Page p = new Page(this.lastId,name, admin);
+		listeSommet.add(new Page(this.lastId, name, admin));
+		lastId++;
+		return p;
 	}
 	
 	public void addPage(int id, String name, User admin){
@@ -91,5 +100,229 @@ public class Graphe {
 		return this.name;
 	}
 	
+	public int getNbArc() {
+		int nbArc = 0;
+		for (Sommet s : this.listeSommet) {
+			nbArc += s.getV_sortant().size();
+		}
+		return nbArc;
+	}
 	
+	public ArrayList<Sommet> getSommetByName() {
+		ArrayList<Sommet> ordered = this.listeSommet;
+		Collections.sort(ordered, new NameComparator());
+		return ordered;
+	}
+	
+	public ArrayList<Sommet> getSommetByDegree() {
+		ArrayList<Sommet> ordered = this.listeSommet;
+		Collections.sort(ordered, new IdComparator());
+		return ordered;
+	}
+	
+	public Hashtable<Sommet, LinkedList<Sommet>> getArc() {
+		Hashtable<Sommet, LinkedList<Sommet>> list = new Hashtable<Sommet, LinkedList<Sommet>>();
+
+		for (Sommet s : this.listeSommet) {
+			if (s instanceof User) {
+				LinkedList<Sommet> temp = s.getV_sortant();
+				list.put(s, temp);
+			}
+
+		}
+
+		return list;
+	}
+	
+	public Sommet getSommet(String name) {
+
+		for (Sommet s : this.listeSommet) {
+			if (s.getName().equals(name)) {
+				return s;
+			}
+		}
+		return null;
+	}
+	
+	public Sommet getSommet(int id) {
+		for (Sommet s : this.listeSommet) {
+			if (s.getNumero() == id) {
+				return s;
+			}
+		}
+		return null;
+	}
+
+	public float getAgeMoyen() {
+		int ageM = 0;
+		for (Sommet s : this.listeSommet) {
+			if (s instanceof User) {
+				ageM += ((User) s).getAge();
+			}
+		}
+		return ageM / this.getNbUser();
+	}
+	
+	public HashSet<User> getAdminUsers() {
+		HashSet<User> listeAdmins = new HashSet<User>();
+		for(Sommet s : this.listeSommet){
+			if(s instanceof Page){
+				listeAdmins.add( ((Page) s).getAdmin() );
+			}
+		}
+		return listeAdmins;
+	}
+	
+	public void deleteSommet(int id) {
+		Sommet sToDelete = this.getSommet(id);
+
+		for (Sommet s : this.listeSommet) {
+			s.getV_sortant().remove(sToDelete);
+		}
+
+		this.listeSommet.remove(sToDelete);
+
+	}
+	
+	public static Graphe loadGraphe(String fileName) {
+		String[] fileNames = {"users.txt", "pages.txt", "relation_user_page.txt"};
+		//this.listeSommet.clear();
+		Graphe g = new Graphe(fileName);
+		
+		BufferedReader br = null;
+		try {
+			for(int i=0; i<fileNames.length; i++){
+				br = new BufferedReader(new FileReader(g.name + "-" + fileNames[i]));
+				String line;
+				
+				while((line = br.readLine()) != null){
+					String[] infos = line.split("[|]");
+					switch(i){
+					case 0:
+						g.addUser(Integer.parseInt(infos[0]), Integer.parseInt(infos[2]), infos[1]);
+						break;
+						
+					case 1:
+						g.addPage(Integer.parseInt(infos[0]), infos[1], (User) g.getSommet(Integer.parseInt(infos[2])));
+						break;
+						
+					case 2:
+						for(int y=1; y<infos.length; y++){
+							User temp = (User) g.getSommet(Integer.parseInt(infos[0]));
+							if(g.getSommet(Integer.parseInt(infos[y])) instanceof User)
+								temp.ajouter_ami((User) g.getSommet(Integer.parseInt(infos[y])));
+							else 
+								temp.like_page((Page) g.getSommet(Integer.parseInt(infos[y])));
+						}
+						break;
+					}
+				}
+				
+			}
+			
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Le fichier n'existe pas");
+			return null;
+		} catch (IOException e) {
+			System.out.println("Erreur lors de la lecture du fichier");
+			return null;
+		}finally{
+			try {
+				br.close();
+			} catch (Exception e) {
+				return null;
+			}
+		}
+		return g;
+	}
+	
+	public void deleteArc(User orig, Sommet cible) {
+		User t = (User) this.getSommet(orig.getName());
+		if(t != null){
+			if(cible instanceof Page){
+				t.unlike_page((Page)cible);
+			}
+			else if(cible instanceof User){
+				t.supprimer_ami((User) cible);
+			}
+		}
+	}
+	
+	// Stocke toutes les relations des utilisateurs dans le fichier nommé
+		// relation_user_page.txt
+		public void relationship() throws IOException {
+
+			BufferedWriter b = new BufferedWriter(new FileWriter(this.name + "-relation_user_page.txt"));
+
+			for (Map.Entry<Sommet, LinkedList<Sommet>> entry : this.getArc().entrySet()) {
+				b.write(entry.getKey().getNumero() + "|");
+				LinkedList<Sommet> sommets = entry.getValue();
+				for (Sommet s : sommets) {
+					if (s instanceof User)
+						b.write(s.getNumero() + "|");
+					else if (s instanceof Page)
+						b.write(s.getNumero() + "|");
+				}
+				b.write("\n");
+			}
+			b.close();
+		}
+	
+		// Stockage de tous les users de l'application dans le fichier users.txt et dans pages.txt pour les pages; chacun des fichiers contient toutes les infos relatives à chaque element
+		public void users_pages() throws IOException {
+			BufferedWriter b = new BufferedWriter(new FileWriter(this.name + "-users.txt"));
+			BufferedWriter c = new BufferedWriter(new FileWriter(this.name + "-pages.txt"));
+			for(Sommet x : this.listeSommet){
+				if(x instanceof Page){
+					c.write(x + "|" + ((Page)x).getAdmin().getNumero() + "\n");
+				}
+				else if(x instanceof User){
+					b.write(x + "\n");
+				}
+			}
+			b.close();c.close();
+		}
+		
+		public Hashtable<Sommet, Integer> getDistanceBetweenSommets(Sommet s){
+			Hashtable<Sommet, Integer> dist = new Hashtable<Sommet, Integer>();
+			ArrayList<Sommet> listSommets = new ArrayList<Sommet>(this.listeSommet);
+			for(Sommet tempS : listSommets){
+					dist.put(tempS, 10000000);
+			}
+			dist.put(s, 0);
+			
+			while(!listSommets.isEmpty()){
+				Sommet plusPetitSommet = null;
+				
+				for(Sommet tempS : listSommets){
+					if(plusPetitSommet == null){
+						plusPetitSommet = tempS;
+					}
+					else if(dist.get(tempS) < dist.get(plusPetitSommet)){
+						plusPetitSommet = tempS;
+					}
+				}
+					
+					listSommets.remove(plusPetitSommet);
+					for(Sommet amiS : plusPetitSommet.getV_sortant()){
+						int d = dist.get(plusPetitSommet) + 1;
+						if(d <= dist.get(amiS)){
+							dist.put(amiS, d);
+						}
+					}
+				
+			}
+			
+			
+			return dist;
+		}
+		
+		public void addRelation(User u, Sommet s){
+			if(s instanceof User)
+				u.ajouter_ami((User) s);
+			else 
+				u.like_page((Page) s);
+		}
 }
--- src/Reseau.java
+++ src/Reseau.java
@@ -2,120 +2,302 @@ import java.io.*;
 import java.util.*;
 import java.util.Map.Entry;
 
-
 public class Reseau {
 
 	/**
 	 * @param args
-	 * @throws IOException 
+	 * @throws IOException
 	 */
 	public static void main(String[] args) throws IOException {
-		Admin ad = new Admin();
-		
-/*		
-		//Test ajout d'utilisateur + affichage info + recuperation User par son nom
-		ad.addUser(20, "hjkl");
-		ad.addUser(30, "ghjk");
-		User ten = (User) ad.getSommet("hjkl");
-		System.out.println("Nom : " + ten.getName() + ", Age : " + ten.getAge() +" Index "+ten.getNumero());
-		
-		User u = new User(2, 15, "azerty");
-		ad.addUser(u);
-		
-		// Test ajout amis
-		ten.ajouter_ami(u);
-		ten.ajouter_ami(u);
-		System.out.println();
-		
-		//Test ajout page + like page
-		ad.addPage("Page1", ten);
-		ad.addPage("facebook", ten);
-		Page p = (Page) ad.getSommet("Page1");
-		ten.like_page(p);
-		
-		
-		System.out.println("\n---------Triage par nom---------");
-		for(Sommet s : ad.getSommetByName()){
-			System.out.println(s.getName() + "| id : " + s.getNumero());
-		}
-		ad.addPage("Page2", ten);
-		ad.addPage("algo", u);
-		System.out.println();
-		affichageArcs(ad);
-		
-		
-		System.out.println("Nombre d'arcs : " + ad.getNbArc());
-		
-		
-			ad.relationship();
-			ad.users_pages();
+		Scanner sc = new Scanner(System.in);
+
+		Graphe g = choixGraphe();
+		choixAction(g);
+
+	}
+
+	public static Graphe choixGraphe() {
+		Scanner sc = new Scanner(System.in);
+		int choix = 0;
+		Graphe g = null;
+		String name;
+
+		do {
+			do {
+				System.out.println("Choix d'une action : ");
+				System.out.println("1/Creer un nouveau graphe");
+				System.out.println("2/Charger un graphe");
+				try {
+					choix = Integer.parseInt(sc.next());
+				} catch (Exception e) {
+					choix = -1;
+				}
+
+			} while (choix < 1 || choix > 2);
+
+			switch (choix) {
+			case 1:
+				System.out.println("Nom du nouveau graphe : ");
+				name = sc.next();
+				g = new Graphe(name);
+				break;
+
+			case 2:
+				System.out.println("Nom du graphe a charger : ");
+				name = sc.next();
+				g = Graphe.loadGraphe(name);
+				break;
+			}
+
+		} while (g == null);
+
+		return g;
+	}
+
+	public static void choixAction(Graphe g) {
+		int choix = -1, age = 0, idOrig = 0, idCible =0, idASup = 0, idAdmin = 0;
+		String name;
+		Scanner sc = new Scanner(System.in);
+
+		do {
+			System.out.println("Choix d'une action : ");
+			System.out.println("1/Ajouter Utilisateur");
+			System.out.println("2/Ajouter Page");
+			System.out.println("3/Afficher le nombre de Sommets");
+			System.out.println("4/Afficher le nombre d'arcs");
+			System.out.println("5/Afficher l'ensemble des Sommets");
+			System.out.println("6/Afficher l'ensemble des Sommets tries par nom");
+			System.out.println("7/Afficher l'ensemble des Sommets tries par degre sortant");
+			System.out.println("8/Afficher l'ensemble des Arcs");		
+			System.out.println("9/Supprimer un Sommet");
+			System.out.println("10/Supprimer un arc");
+			System.out.println("11/Afficher le nombre d'Utilisateurs");
+			System.out.println("12/Afficher le nombre de Pages");
+			System.out.println("13/Afficher l'ensemble des Utilisateurs etant administrateur de page");
+			System.out.println("14/Afficher les informations d'un Sommet");
+			System.out.println("15/Ajouter relation");
+			System.out.println("16/Enregistrer le graphe");
+			System.out.println("17/Quitter");
+			try {
+				choix = Integer.parseInt(sc.next());
+			} catch (Exception e) {
+				choix = -1;
+			}
 			
-		System.out.println("----------Fin Save---------");
-		ad.loadGraphe("test2");
-		affichageArcs(ad);
-		System.out.println("\n---------Triage par nom---------");
-		for(Sommet s : ad.getSommetByName()){
-			System.out.println(s.getName() + "| id : " + s.getNumero() + " User? : " + (s instanceof User));
-		}
-		System.out.println("-----");
-		for(Sommet s : ad.getSommetByDegree()){
-			System.out.println(s.getName() + "| id : " + s.getNumero() + " User? : " + (s instanceof User));
-		}
-		System.out.println("-----");
+			sc.nextLine();
+			
+			switch (choix) {
+			case 1:
+				System.out.println("Nom du nouvel utilisateur : ");
+				name = sc.nextLine();
+				
+				do {
+					System.out.println("Age du nouvel utilisateur : ");
+					try {
+						age = Integer.parseInt(sc.next());
+					} catch (Exception e) {
+						age = -1;
+					}
+				} while (age < 0);
+				User u = g.addUser(age, name);
+				//choixActionUser(u);
+				break;
+				
+			case 2:
+				System.out.println("Nom de la nouvelle page : ");
+				String namePage = sc.nextLine();
+				idAdmin = -1;
+				
+				do {
+					System.out.println("Id de l'Utilisateur administrant cette page : ");
+					try {
+						idAdmin = Integer.parseInt(sc.next());
+					} catch (Exception e) {
+						idAdmin = -1;
+					}finally{
+						if(g.getSommet(idAdmin) == null || !(g.getSommet(idAdmin) instanceof User)){
+							System.out.println("Cet Utilisateur n'existe pas");
+							idAdmin = -1;
+						}
+					}
+				} while (idAdmin < 0);
+				Page p = g.addPage(namePage, (User) g.getSommet(idAdmin));
+				break;
+				
+			case 3:
+				System.out.println("Nombre de Sommet total : " + g.getNbSommet());
+				break;
+			case 4:
+				System.out.println("Nombre d'Arcs total : "+ g.getNbArc());
+				break;
+			case 5:
+				System.out.println("Ensemble des sommets : ");
+				for(Sommet s : g.getListeSommet()){
+					System.out.println(s.getName() + " -> id : " + s.getNumero() + ", nom : " + s.getName() + ", de type : " + s.getClass());
+				}
+				break;
+			case 6:
+				System.out.println("Ensemble des sommets tries par nom : ");
+				for(Sommet s : g.getSommetByName()){
+					System.out.println(s.getName() + " -> id : " + s.getNumero() + ", nom : " + s.getName() + ", de type : " + s.getClass());
+				}
+				break;
+				
+			case 7:
+				System.out.println("Ensemble des sommets pas degre sortant : ");
+				for(Sommet s : g.getSommetByDegree()){
+					System.out.println(s.getName() + " -> id : " + s.getNumero() + ", nom : " + s.getName() + ", de type : " + s.getClass());
+				}
+				break;
+				
+			case 8:
+				for (Map.Entry<Sommet, LinkedList<Sommet>> entry : g.getArc().entrySet()) {
+					System.out.print(entry.getKey().getName());
+					LinkedList<Sommet> sommets = entry.getValue();
+					for (Sommet s : sommets) {
+						System.out.print(" -> " + s.getName());
+					}
+					System.out.println();
+				}
+				break;
+				
+			case 9:
+				idASup = -1;
+				System.out.println("Id du sommet à supprimer : ");
+				do {
+					try {
+						idASup = Integer.parseInt(sc.next());
+					} catch (Exception e) {
+						idASup = -1;
+					}
+				} while (idASup < 0);
+				g.deleteSommet(idASup);
+				break;
+				
+			case 10:
+				idOrig = -1;
+				System.out.println("Id du sommet à l'origine du lien : ");
+				do {
+					try {
+						idOrig = Integer.parseInt(sc.next());
+					} catch (Exception e) {
+						idOrig = -1;
+					}
+				} while (idOrig < 0);
+				
+				idCible = -1;
+				System.out.println("Id du sommet à supprimer des voisins sortants : ");
+				do {
+					try {
+						idCible = Integer.parseInt(sc.next());
+					} catch (Exception e) {
+						idCible = -1;
+					}
+				} while (idCible < 0);
+				
+				if(g.getSommet(idOrig) instanceof User)
+					g.deleteArc((User)g.getSommet(idOrig), g.getSommet(idCible));
+				else
+					System.out.println("Merci de choisir l'ID d'un utilisateur lors du premier choix");
+				break;
+				
+			case 11:
+				System.out.println("Nombre d'utilisateurs : " + g.getNbUser());
+				break;
+				
+			case 12:
+				System.out.println("Nombre d'utilisateurs : " + g.getNbPage());
+				break;
+			
+			case 13:
+				System.out.println("Ensemble des Utilisateurs etant administrateurs de Page : ");
+				for(Sommet s : g.getAdminUsers()){
+					System.out.println(s.getName() + " -> id : " + s.getNumero() + ", nom : " + s.getName() + ", de type : " + s.getClass());
+				}
+				break;
+				
+			case 14:
+				System.out.println("Nom du Sommet : ");
+				String nameSommet = sc.nextLine();
+				if(g.getSommet(nameSommet) != null){
+					Sommet tempS = g.getSommet(nameSommet);
+					System.out.println("Nom du sommet : " + tempS.getName());
+					System.out.println("Id : " + tempS.getNumero());
+					if(tempS instanceof User){
+						System.out.println("Age : " + ((User)tempS).getAge());
+						System.out.println("Amis et page aimees de ce Sommet : ");
+						for(Sommet s : ((User)tempS).getV_sortant()){
+							System.out.print(s.getName() + ", ");
+						}
+						System.out.println();
+					}
+					else if(tempS instanceof Page){
+						System.out.println("Administrateur de ce Sommet : " + ((Page)tempS).getAdmin().getName() + " (Id : " + ((Page)tempS).getAdmin().getNumero() + ")");
+					}
+				}
+				else{
+					System.out.println("Ce sommet n'existe pas");
+				}
+				break;
+				
+			case 15:
+				idOrig = -1;
+				
+				do {
+					System.out.println("Id du sommet à l'origine du lien : ");
+					try {
+						idOrig = Integer.parseInt(sc.next());
+					} catch (Exception e) {
+						idOrig = -1;
+					}
+				} while (idOrig < 0);
+				
+				idCible = -1;
+				
+				do {
+					System.out.println("Id du sommet Cible : ");
+					try {
+						idCible = Integer.parseInt(sc.next());
+					} catch (Exception e) {
+						idCible = -1;
+					}
+				} while (idCible < 0 || idCible == idOrig);
+				
+				if(g.getSommet(idOrig) instanceof User)
+					g.addRelation((User) g.getSommet(idOrig), g.getSommet(idCible));
+				else
+					System.out.println("Merci de choisir l'ID d'un utilisateur lors du premier choix");
+				break;
+				
+			case 16:
+				try {
+					g.users_pages();
+					g.relationship();
+				} catch (IOException e) {
+					System.out.println("Une erreure est survenue lors de l'enregistrement des donnees");
+				}			
+				break;
+			}
+			System.out.println("----------------------------------");
+		} while (choix != 17);
 		
-		affichageArcs(ad);
-		ad.deleteArc(ten, u);
-		ad.deleteArc(ten, ad.getSommet("Page1"));
-		affichageArcs(ad);
-		System.out.println("--Liste admins de page---");
-		for(User ua : ad.getAdminUsers()){
-			System.out.println(ua.getName());
-		}
-*/		
+		System.out.println("FIN");
 		
-		
-		ad.loadGraphe("test2");
-		
-		System.out.println("-----------------");
-		for(Sommet s : ad.getSommetByName()){
-			System.out.println(s.getName() + "| id : " + s.getNumero() + " User? : " + (s instanceof User));
-		}
-		System.out.println("-----------------");
-		for(Sommet s : ad.getSommet()){
-			System.out.println(s.getName() + "| id : " + s.getNumero() + " User? : " + (s instanceof User));
-		}
-		System.out.println("-----------------");
-		affichageArcs(ad);
-		System.out.println("-----------------\n");
-		
-		affichageArcs(ad);
-		
-		affichageDistance(ad, ad.getSommet(14));
+
+	}
+
+	public static void choixActionUser(User u) {
 		
 	}
-	
-	// recuperation des arcs et affichage
-	public static void affichageArcs(Admin ad){
-		for (Map.Entry<Sommet, LinkedList<Sommet>> entry : ad.getArc().entrySet()){
-		     System.out.print(entry.getKey().getName()); 
-		     LinkedList<Sommet> sommets = entry.getValue();
-			 for(Sommet s : sommets){
-				 System.out.print(" -> " + s.getName());
-			 }
-			 System.out.println();
-		}
-	}
-	
-	public static void affichageArcs(User u){
-		for( Sommet s : u.getV_sortant()){
-			System.out.println(s.getName());
-		}
-	}
-	
-	public static void affichageDistance(Admin ad, Sommet s){
-		System.out.println("---Distances entre " + s.getName() + " et tous les autres sommets");
-		for (Entry<Sommet, Integer> entry : ad.getDistanceBetweenSommets(s).entrySet()){
-		      System.out.println(entry.getKey().getName() + " -> " + entry.getValue());
+
+
+	public static void affichageDistance(Graphe g, Sommet s) {
+		System.out.println("---Distances entre " + s.getName()
+				+ " et tous les autres sommets");
+		for (Entry<Sommet, Integer> entry : g.getDistanceBetweenSommets(s)
+				.entrySet()) {
+			System.out.println(entry.getKey().getName() + " -> "
+					+ entry.getValue());
 		}
 	}
 
